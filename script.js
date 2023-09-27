de2a7b38cc5577d805fee825bf6646d611f5615d
Shah Nawaz Unar <shahnawazunar85@gmail.com>
2023-09-27 00:56:30 -0700

    on Click randon color Change

---
+++ script.js
@@ -0,0 +1,18 @@
+const btnChange = document.getElementById("btnChange");
+const containerBox = document.getElementById("container");
+
+let randomColor = ()=>{
+    return Math.floor(Math.random()*255);
+};
+
+let clrChanger = ()=>{
+    let randonNumber =`rgb(${ randomColor()},${ randomColor()},${ randomColor()})`;
+
+    containerBox.style.backgroundColor = randonNumber;
+    btnChange.style.color = randonNumber;
+
+
+};
+btnChange.addEventListener("click",clrChanger);
+
+
